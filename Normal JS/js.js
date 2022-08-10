d07357d01a3fa8b13718d340cabedc6b1b29f2b5
Messi002 <rostandmessi2@gmail.com>
2022-08-10 23:21:56 +0100

    dsa

---
+++ Normal JS/js.js
@@ -0,0 +1,10 @@
+let ans = null;
+
+console.log(ans);
+fullName = "Hello World"
+
+console.log(fullName[3]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase()); //Methods end with ()
+
+
