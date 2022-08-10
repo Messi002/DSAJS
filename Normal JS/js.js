a6928106a10e503aaf42d1ac85b2d4690226d267
Messi002 <rostandmessi2@gmail.com>
2022-08-10 23:32:15 +0100

    dsa

--- Normal JS/js.js
+++ Normal JS/js.js
@@ -2,9 +2,22 @@ let ans = null;
 
 console.log(ans);
 fullName = "Hello World"
+email = "larethgang@gmail.com.cm"
 
 console.log(fullName[3]);
 console.log(fullName.length);
 console.log(fullName.toUpperCase()); //Methods end with ()
 
+let index = email.indexOf("@"); 
+console.log(index);
 
+// let result = email.lastIndexOf("l");
+// console.log(result);
+
+//slice, sliice a section from the string
+
+// let result = email.slice(0,10);
+// console.log(result);
+
+let result = email.slice(0,10);
+// console.log(result);
