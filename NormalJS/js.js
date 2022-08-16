e7a48f301a0ec1305983ccb07d5b593b42507cac
Messi002 <rostandmessi2@gmail.com>
2022-08-16 23:17:14 +0100

    dsa

--- NormalJS/js.js
+++ NormalJS/js.js
@@ -1,7 +1,24 @@
-button = document.querySelector("button");
-popup = document.querySelector(".wrapper");
-closeup = document.querySelector(".close");
+// button = document.querySelector("button");
+// popup = document.querySelector(".wrapper");
+// closeup = document.querySelector(".close");
 
-button.addEventListener('click', () =>{
- popup.style.display = "block";
-});
+// button.addEventListener('click', () =>{
+//         console.log('Button fired here!');
+//  popup.style.display = "block";
+// });
+
+// closeup.addEventListener('click', () =>{
+//         console.log('closeup fired here!');
+//         popup.style.display = "none";
+//        });
+
+// popup.addEventListener('click', () =>{
+//         console.log('popup fired here!');
+//         popup.style.display = "none";
+//        });
+
+form = document.querySelector(".signup-form");
+form.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log( form.username.value);
+})
