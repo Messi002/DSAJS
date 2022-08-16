d7fb7ef5150ba71f78e1f5f2fbba6d3952d76535
Messi002 <rostandmessi2@gmail.com>
2022-08-16 22:51:38 +0100

    dsa

---
+++ NormalJS/js.js
@@ -0,0 +1,7 @@
+button = document.querySelector("button");
+popup = document.querySelector(".wrapper");
+closeup = document.querySelector(".close");
+
+button.addEventListener('click', () =>{
+ popup.style.display = "block";
+});
