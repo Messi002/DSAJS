fba27467f1631fc57faad52370015a6b066768dc
Messi002 <rostandmessi2@gmail.com>
2022-08-17 21:55:55 +0100

    dsa

--- Js/js3.js
+++ Js/js3.js
@@ -1,2 +1,16 @@
 //Filter Method on Arrays
-const scores =[10,30,15,25,50,40,5];
+// const scores =[10,30,15,25,50,40,5];
+
+// const Users =[
+//     {name: "Austin", premium: true},
+//     {name: "Shelly", premium: false},
+//     {name: "Luigi", premium: true},
+//     {name: "Shaun", premium: false}
+// ];
+
+// const users = Users.filter(user => user.premium);
+
+
+// console.log(users);
+
+
