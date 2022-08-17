7c997db7a94d8c1086daca7efd8d643675683bda
Messi002 <rostandmessi2@gmail.com>
2022-08-17 20:47:28 +0100

    dsa

--- NormalJS/js.js
+++ NormalJS/js.js
@@ -51,3 +51,22 @@
 //         }
 // })
 
+
+const correctAnswers = ['B','B','B','B'];
+const form = document.querySelector('.quiz-form');
+const answerDiv = document.querySelector(".answer-here");
+
+form.addEventListener('submit', e =>{
+ e.preventDefault();
+ 
+ let score = 0;
+ const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+console.log(userAnswers);
+ userAnswers.forEach( (answer, index)=>{
+        if (answer === correctAnswers[index]) {
+                score +=25;
+        }
+ } )
+
+answerDiv.textContent= `${score}`;
+});
