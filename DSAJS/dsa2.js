3e44a9f813a56fb3778f4b1d4b990af386995efd
Messi002 <rostandmessi2@gmail.com>
2022-08-18 20:37:29 +0100

    dsa

--- DSAJS/dsa2.js
+++ DSAJS/dsa2.js
@@ -16,15 +16,26 @@ class HashTable{
 
 _hash(key){
     let hash = 0;
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < key.length; i++) {
         hash = (hash + key.charCodeAt(i) * i)%
         this.data.length        
     }
     return hash;
 }
+
+set(keys, value) {
+  let address = this._hash(keys);
+    if (!this.data[address]) {
+    this.data[address]=[];
+    }
+    this.data[address].push([keys, value]);
+    return this.data
+}
 }
 
 const myHashTable = new HashTable(50);
-console.log(myHashTable._hash("grapes"));
+// console.log(myHashTable._hash("grapes"));
 myHashTable.set('grapes',10000);
-myHashTable.get('grapes');
+// myHashTable.set('mangoes',400);
+// myHashTable.set('apples',100);
+// myHashTable.get('grapes');
