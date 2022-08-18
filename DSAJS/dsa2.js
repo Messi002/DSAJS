d4a0ecea1456b823818eaa98ff0035b1fa1b9079
Messi002 <rostandmessi2@gmail.com>
2022-08-18 20:01:49 +0100

    dsa

--- DSAJS/dsa2.js
+++ DSAJS/dsa2.js
@@ -1,7 +1,9 @@
-let user = {
-    name: "kylie"
+// let user = {
+//     name: "kylie",
+//     color: ['blue','red']
 
-}
-console.log(user);
-user.age=20;
-console.log(user);
+// }
+// console.log(user);
+// user.age=20;
+// console.log(user);
+// console.log(user.color[1]);
