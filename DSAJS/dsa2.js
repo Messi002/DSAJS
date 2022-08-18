8d0664e826960cd2ca2613419de15f1a79a356ab
Messi002 <rostandmessi2@gmail.com>
2022-08-18 20:10:44 +0100

    dsa

--- DSAJS/dsa2.js
+++ DSAJS/dsa2.js
@@ -7,3 +7,24 @@
 // user.age=20;
 // console.log(user);
 // console.log(user.color[1]);
+
+
+class HashTable{
+    constructor(size){
+        this.data = new Array(size);
+    }
+
+_hash(key){
+    let hash = 0;
+    for (let i = 0; i < array.length; i++) {
+        hash = (hash + key.charCodeAt(i) * i)%
+        this.data.length        
+    }
+    return hash;
+}
+}
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable._hash("grapes"));
+myHashTable.set('grapes',10000);
+myHashTable.get('grapes');
