50c27b8cdcc2f03ef069a9f325cd2f2a70eeef80
Messi002 <rostandmessi2@gmail.com>
2022-08-21 10:50:45 +0100

    dsa

--- Js/js3.js
+++ Js/js3.js
@@ -196,3 +196,32 @@ const search = document.querySelector(".search > input");
 // })
 
 //********************************************** */
+
+
+//********************************************** */
+// Weather Pasrt
+
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.key);
+
+    return {
+        cityDets : cityDets,
+        weather : weather
+    }
+}
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const city= cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the ui with new city
+    updateCity(city)
+    .then( data => console.log( data))
+    .catch( err => console.log(err))
+})
+
+//********************************************** */
