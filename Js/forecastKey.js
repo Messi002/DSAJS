988a9004233d41b393c3da17948e60651e37ff4e
Messi002 <rostandmessi2@gmail.com>
2022-08-21 10:19:03 +0100

    dsa

--- Js/forecastKey.js
+++ Js/forecastKey.js
@@ -1,1 +1,5 @@
-const key = 'keyherepleasedontforget'
+const key = 'keyherepleasedontforget';
+
+const getCity = async () => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+}
