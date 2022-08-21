51ba42fe65e1b0d1d26f073f22eb0977586c46a4
Messi002 <rostandmessi2@gmail.com>
2022-08-21 10:29:21 +0100

    dsa

--- Js/forecastKey.js
+++ Js/forecastKey.js
@@ -1,5 +1,28 @@
 const key = 'keyherepleasedontforget';
 
-const getCity = async () => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//get weather information
+
+const getWeather = async(id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `?apikey=${key}&a=${city}`;
+
 }
+
+
+
+//get city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&a=${city}`;
+
+    const res = await fetch(base + query);
+
+    const data = await res.json();
+
+    return data[0]
+}
+
+
+getCity('manchester')
+ .then(data => console.log(data))
+ .catch(err => console.log(err));
