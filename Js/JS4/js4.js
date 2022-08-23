41306e5d3cdad5aa1bd4ab54010e465806bbebc6
Messi002 <rostandmessi2@gmail.com>
2022-08-23 19:54:59 +0100

    dsa

--- Js/JS4/js4.js
+++ Js/JS4/js4.js
@@ -22,9 +22,26 @@ class User{
 }
 
 const userOne = new User();
+const userThree = new User('Gareth');
+const userSix = new User('Rareth');
+const userFour = new User('Dareth');
+const userFive = new User('Mareth');
 console.log(userOne.name, userOne.email);
 const userTwo = new User('Lareth','lareth@mirsquared.com');
 console.log(userTwo.name,userTwo.email);
 userOne.login().logout().incScore().incScore();
 userTwo.login();
+
+let users = [userOne, userTwo,userThree,userFour,userFive,userSix];
+
+class Admin extends User{
+    deleteUser(user){
+       const userNum= users.filter(userCheck => userCheck != user);
+      console.log(userNum);
+    }
+}
+
+const AdminOne = new Admin();
+
+AdminOne.deleteUser(userFour)
 
