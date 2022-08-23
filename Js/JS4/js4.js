12f87df25cdfb52139ede62b5c8f4b49def0429c
Messi002 <rostandmessi2@gmail.com>
2022-08-23 18:55:40 +0100

    dsa

---
+++ Js/JS4/js4.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(name,email){
+        this.name='Austin',
+        this.email='Austin@mirsquared.cm'
+    }
+}
+
+const userOne = new User();
+console.log(userOne.name, userOne.email);
