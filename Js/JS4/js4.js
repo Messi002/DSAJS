d36eb9063cc3c45896babee5be04ffeb920852b1
Messi002 <rostandmessi2@gmail.com>
2022-08-23 21:13:02 +0100

    dsa

--- Js/JS4/js4.js
+++ Js/JS4/js4.js
@@ -24,34 +24,43 @@
 function User(username='Austin', email='Austin@mirsquared.cm') {
     this.username = username;
     this.email = email;
-    this.login = function (params) {
-        console.log(`${this.username} has logged in`);
-    }
+    
 }
 
+User.prototype.login = function (params) {
+    console.log(`${this.username} has logged in`);
+    return this;
+}
 
+User.prototype.logout = function (params) {
+    console.log(`${this.username} has logged out`);
+    return this;
+}
 
-const userOne = new User();
-const userThree = new User('Gareth');
-const userSix = new User('Rareth');
-const userFour = new User('Dareth');
-const userFive = new User('Mareth');
-console.log(userOne.name, userOne.email);
-const userTwo = new User('Lareth','lareth@mirsquared.com');
-console.log(userTwo.name,userTwo.email);
+const userOne = new User('Lareth','Lareth@mirsquared.cm');
+console.log(userOne);
+
+// const userOne = new User();
+// const userThree = new User('Gareth');
+// const userSix = new User('Rareth');
+// const userFour = new User('Dareth');
+// const userFive = new User('Mareth');
+// console.log(userOne.name, userOne.email);
+// const userTwo = new User('Lareth','lareth@mirsquared.com');
+// console.log(userTwo.name,userTwo.email);
 // userOne.login().logout().incScore().incScore();
-userTwo.login();
+// userTwo.login();
 
-let users = [userOne, userTwo,userThree,userFour,userFive,userSix];
+// let users = [userOne, userTwo,userThree,userFour,userFive,userSix];
 
-class Admin extends User{
-    deleteUser(user){
-       const userNum= users.filter(userCheck => userCheck != user);
-      console.log(userNum);
-    }
-}
+// class Admin extends User{
+//     deleteUser(user){
+//        const userNum= users.filter(userCheck => userCheck != user);
+//       console.log(userNum);
+//     }
+// }
 
-const AdminOne = new Admin();
+// const AdminOne = new Admin();
 
 // AdminOne.deleteUser(userSix)
 
