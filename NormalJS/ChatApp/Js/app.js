409cb667a308549a52b33a0e8fdfd67d6f6f8efe
Messi002 <rostandmessi2@gmail.com>
2022-08-24 22:33:53 +0100

    dsa

--- NormalJS/ChatApp/Js/app.js
+++ NormalJS/ChatApp/Js/app.js
@@ -5,6 +5,9 @@ const newChatForm = document.querySelector(".new-chat");
 //add a new chat
 newChatForm.addEventListener('submit', e => {
     const msg = newChatForm.message.value.trim();
+    chatroom.addChats(msg)
+     .then(() => newChatForm.reset() )
+     .catch((err) => console.log(err));
 })
 
 //class instances
