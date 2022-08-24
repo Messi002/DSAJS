04be4b8f7b5558b46fa395bbe997304a90a83f4e
Messi002 <rostandmessi2@gmail.com>
2022-08-24 22:47:11 +0100

    dsa

--- NormalJS/ChatApp/Js/app.js
+++ NormalJS/ChatApp/Js/app.js
@@ -1,9 +1,26 @@
 const chatlist = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMsg= document.querySelector(".update-msg");
+
+
+//add a new name
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    //show then hide update message
+    updateMsg.innerText = `Your name was updated to ${newName}`;
+
+    setTimeout(()=> updateMsg.innerText = "",3000)
+
+})
 
 
 //add a new chat
 newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
     const msg = newChatForm.message.value.trim();
     chatroom.addChats(msg)
      .then(() => newChatForm.reset() )
