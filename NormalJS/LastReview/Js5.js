1b432a7ad1a1edf64b0ca1124ca9fb2f6dd910b8
Messi002 <rostandmessi2@gmail.com>
2022-08-26 23:14:36 +0100

    dsa

--- NormalJS/LastReview/Js5.js
+++ NormalJS/LastReview/Js5.js
@@ -1,22 +1,22 @@
 //rest syntax
-const double = (...nums) => {
-    console.log(nums);
+// const double = (...nums) => {
+//     console.log(nums);
 
 
-    return this;
-}
-const results = double(1,2,3,4,5)
+//     return this;
+// }
+// const results = double(1,2,3,4,5)
 
 // spread syntax Array
-const people = ['shaun','Gareth','Lareth'];
-console.log(...people);
+// const people = ['shaun','Gareth','Lareth'];
+// console.log(...people);
 
 //spread syntax Objects
-const person = {
-    name: "Austin",
-    color: "red",
-}
+// const person = {
+//     name: "Austin",
+//     color: "red",
+// }
 
-const personClone = {...person}
-personClone.color = 'blue'
-console.log(personClone);
+// const personClone = {...person}
+// personClone.color = 'blue'
+// console.log(personClone);
